46976b5c11419d117dc97380e13ace689a5f35c0
Aredu89 <aredu_r89@hotmail.com>
2026-02-20 15:42:26 -0600

    Add and inject application properties

--- src/main/java/com/springboot/demo/SpringDemo/rest/DemoRestController.java
+++ src/main/java/com/springboot/demo/SpringDemo/rest/DemoRestController.java
@@ -1,13 +1,17 @@
 package com.springboot.demo.SpringDemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
     //Expose a "/" endpoint that returns "Hello World"
+    @Value("${team.name}")
+    String teamName;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return "Hello World" + " - " + teamName;
     }
 }
